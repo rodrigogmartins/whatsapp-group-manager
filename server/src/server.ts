cfc38badb4e130a8f3d0cee1d50f635f566981be
Rodrigo Martins <rodrigo.martins7121@gmail.com>
2020-09-26 10:57:32 -0300

    Commit inicial

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello!' })
+})
+
+app.listen(process.env.PORT)
