2faf7bd16b22e6ad32d7f57156184c4dc612f0bd
Rodrigo Martins <rodrigo.martins7121@gmail.com>
2021-01-03 18:52:21 -0300

    Adicionando rotas para Links

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { UserController } from '@controllers/UserController'
 import { GroupController } from '@controllers/GroupController'
+import { LinkController } from '@controllers/LinkController'
 import { AuthController } from '@controllers/AuthController'
 import { AuthMiddleware } from '@middlewares/AuthMiddleware'
 
@@ -21,5 +22,10 @@ routes
   .post('/api/groups', AuthMiddleware, GroupController.create)
   .put('/api/groups/:id', AuthMiddleware, GroupController.update)
   .delete('/api/groups/:id', AuthMiddleware, GroupController.delete)
+  // Links
+  .get('/api/links', AuthMiddleware, LinkController.index)
+  .post('/api/links', AuthMiddleware, LinkController.create)
+  .put('/api/links/:id', AuthMiddleware, LinkController.update)
+  .delete('/api/links/:id', AuthMiddleware, LinkController.delete)
 
 export { routes }
