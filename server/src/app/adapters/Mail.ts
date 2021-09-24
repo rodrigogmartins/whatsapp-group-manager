f841389abeefc0c0795f25f85f8f4e26588d662c
Rodrigo Martins <rodrigo.martins@ubots.com.br>
2021-09-23 21:57:50 -0300

    Refatorando email adapter

--- server/src/app/adapters/Mail.ts
+++ server/src/app/adapters/Mail.ts
@@ -1,26 +1,29 @@
-import nodemailer from 'nodemailer'
-import dotenv from 'dotenv'
+import nodemailer, { Transporter } from 'nodemailer'
+import SMTPTransport from 'nodemailer/lib/smtp-transport'
 
-dotenv.config()
-
-const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT) || 25,
-  secure: process.env.EMAIL_SECURE === 'true',
-  requireTLS: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-})
-
-interface IEmailOptions {
+interface EmailOptions {
   from: string
   to: string
   subject: string
   html: string
 }
+export class EmailAdapter {
+  private transporter: Transporter<SMTPTransport.SentMessageInfo>
 
-export const sendMail = (emailOptions: IEmailOptions): Promise<any> => {
-  return transporter.sendMail(emailOptions)
+  constructor () {
+    this.transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: Number(process.env.EMAIL_PORT) || 25,
+      secure: process.env.EMAIL_SECURE === 'true',
+      requireTLS: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+      }
+    })
+  }
+
+  send (emailOptions: EmailOptions): Promise<SMTPTransport.SentMessageInfo> {
+    return this.transporter.sendMail(emailOptions)
+  }
 }
