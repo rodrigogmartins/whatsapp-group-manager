db51a75dc12feb43457fbe9b4fbdedb61a93b5cb
rodrigogmartins <rodrigo.martins7121@gmail.com>
2023-09-19 22:02:59 -0300

    WIP: Healthcheck and dockerfile

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,27 @@
+import http from 'http'
 import 'module-alias/register'
 import { app } from '@/app/config/app'
 import { LoggerAdapter } from '@/infra/adapters/Logger'
+import { createTerminus } from '@godaddy/terminus'
 
 const log = LoggerAdapter.createLogFor('server.js')
+const server = http.createServer(app)
 
-app.listen(process.env.PORT, () =>
+createTerminus(server, {
+  signal: 'SIGINT',
+  healthChecks: {
+    '/healthcheck': () => {
+      return new Promise(() => true)
+    }
+  },
+  onSignal: () => {
+    log.info('server is starting cleanup')
+
+    return new Promise(() => true)
+  }
+})
+
+server.on('listening', () => {
   log.info(`Server is running at: http://localhost:${process.env.PORT}`)
-)
+})
+server.listen(process.env.PORT)
