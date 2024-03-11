307341da1a1e37aa086062fba7b73a5b704c7f06
rodrigogmartins <rodrigo.martins7121@gmail.com>
2024-03-11 00:08:26 -0300

    Fix migration

--- server/src/infra/database/migrations/20240310191822_create_users_table.ts
+++ server/src/infra/database/migrations/20240310191822_create_users_table.ts
@@ -4,7 +4,6 @@ export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTableIfNotExists('users', (table) => {
     table.string('id').primary()
     table.string('name').notNullable()
-    table.string('cpf_cnpj').notNullable().unique()
     table.string('email').notNullable().unique()
     table.string('password').notNullable()
     table.timestamps(true, true)
